d1de0e20e0ce672d9c181cb869363970e0064964
MSit <michellesit93@gmail.com>
2017-03-16 14:27:18 -0400

    small

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def login():
     else:
     	return "Get method otherwise I think"
 
-if __name__ == '__main__':
-	app.debug = True
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+#	app.debug = True
+#	port = int(os.environ.get("PORT", 5000))
+#	app.run(host='0.0.0.0', port=port)

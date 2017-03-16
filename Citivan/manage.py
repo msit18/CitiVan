ced15ad4e7d9a4a5630f2f5a3c774f52de87b55e
MSit <michellesit93@gmail.com>
2017-03-16 14:48:08 -0400

    switching to manage.py

---
+++ Citivan/manage.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+    	return "Post method"
+        
+    else:
+    	return "Get method otherwise I think"
+
+#if __name__ == '__main__':
+#	app.debug = True
+#	port = int(os.environ.get("PORT", 5000))
+#	app.run(host='0.0.0.0', port=port)

38f7dc37b8f67c9dbd1faef74da667f570d654ad
MSit <michellesit93@gmail.com>
2017-03-21 16:22:55 -0400

    latest manage file

--- Citivan/manage.py
+++ Citivan/manage.py
@@ -10,26 +10,29 @@ def hello_world():
 
 @app.route('/exchange', methods=['GET', 'PUT'])
 def login():
-	if request.method == 'GET':
-		return "Get method: Hello, World from SCL!"
-	elif request.method == 'PUT':
-		cellNum = request.args.get('cellphoneNum')
-		messageVal = request.args.get('message')
-		print "KEYS: ",cellNum
-		print "MESSAGE: ", messageVal
-		# logs = "Cellphone Number: {0}\nMessage: {1}".format(cellNum, messageVal)
-		if (str(cellNum) != "None") & (str(messageVal) != "None") :
-			returnVal = analyzeSMSInfo(cellNum, messageVal)
-		elif (str(cellNum) == "None"):
-			returnVal = "Sorry, there was an error with recording your cellphone number. Please try again."
-		elif (str(messageVal) == "None"):
-			returnVal = "Sorry, there was an error with the message you sent. Please try again."
+	try:
+		if request.method == 'GET':
+			return "Get method: Hello, World from SCL!"
+		elif request.method == 'PUT':
+			cellNum = request.args.get('cellphoneNum')
+			messageVal = request.args.get('message')
+			print "KEYS: ",cellNum
+			print "MESSAGE: ", messageVal
+			# logs = "Cellphone Number: {0}\nMessage: {1}".format(cellNum, messageVal)
+			if (str(cellNum) != "None") & (str(messageVal) != "None") :
+				returnVal = analyzeSMSInfo(cellNum, messageVal)
+			elif (str(cellNum) == "None"):
+				returnVal = "Sorry, there was an error with recording your cellphone number. Please try again."
+			elif (str(messageVal) == "None"):
+				returnVal = "Sorry, there was an error with the message you sent. Please try again."
+			else:
+				returnVal = "Sorry, there was an error with the message and cellphone number. Please try again!"
+			print "RETURN VAL: ", returnVal
+			return returnVal
 		else:
-			returnVal = "Sorry, there was an error with the message and cellphone number. Please try again!"
-		print "RETURN VAL: ", returnVal
-		return returnVal
-	else:
-		return "This page does not exist!"
+			return "This page does not exist!"
+	except:
+		
 
 def analyzeSMSInfo(ID, msg):
 	s = Server()

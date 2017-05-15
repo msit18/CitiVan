0626e6eecd8e932f1d658dff698c02b27c7574a0
MSit <michellesit93@gmail.com>
2017-05-15 12:16:50 -0400

    Small change

--- Citivan/manage.py
+++ Citivan/manage.py
@@ -5,7 +5,7 @@ import requests
 
 import json
 
-from bs4 import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 
 from analyzeSMSResponses import CitivanSMS
 from analyzeSMSResponses import Server
